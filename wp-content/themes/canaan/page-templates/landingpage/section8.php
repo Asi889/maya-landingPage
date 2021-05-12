f998f4bf0ee7335bbf46bb6b8719702aa0678948
Your username <your_email@gmool.mom>
2021-05-12 14:33:55 +0300

    organized  css folder

--- wp-content/themes/canaan/page-templates/landingpage/section8.php
+++ wp-content/themes/canaan/page-templates/landingpage/section8.php
@@ -9,42 +9,42 @@
 
 
 
-        <div class="flex-row lp_sec9_right mx-10">
+        <div class="flex-row lp_sec8_right mx-10">
 
-            <h1 class="lp-section9-title my-5">צרו עימנו קשר</h1>
+            <h1 class="lp-section8-title my-5">צרו עימנו קשר</h1>
             <p class="lp_sec8_text">יש לכם שאלה?</p>
             <p class="lp_sec8_text">מלאו את הפרטים ונחזור אליכם בהקדם</p>
 
         </div>
 
-        <div class="lp_sec9_left flex-row mx-10">
+        <div class="lp_sec8_left flex-row mx-10">
 
-            <div class="lp_section9_left_radio_container">
+            <div class="lp_section8_left_radio_container">
 
                 <div class="text-mainWhite ">
-                    <input id="input1" type="radio" checked class="lp_sec9_checkbox mx-2" name="look_for_job_education" value="looking">
+                    <input id="input1" type="radio" checked class="lp_sec8_checkbox mx-2" name="look_for_job_education" value="looking">
                     <label for="input1 " class="lp_sec8_text">מחפש עבודה/לימודים</label>
                 </div>
 
                 <div class="text-mainWhite">
-                    <input id="input2" type="radio" class="lp_sec9_checkbox mx-2" name="look_for_job_education" value="eployer">
+                    <input id="input2" type="radio" class="lp_sec8_checkbox mx-2" name="look_for_job_education" value="eployer">
                     <label for="input2" class="lp_sec8_text">מעסיק</label>
                 </div>
 
                 <div class="text-mainWhite">
-                    <input id="input3" type="radio" class="lp_sec9_checkbox mx-2" name="look_for_job_education" value="help">
+                    <input id="input3" type="radio" class="lp_sec8_checkbox mx-2" name="look_for_job_education" value="help">
                     <label for="input3" class="lp_sec8_text">מרכז ייעוץ תעסוקתי</label>
                 </div>
             </div>
 
             <div class="flex-col">
-                <input id="input4" class="lp_sec9_inputs  mx-2 my-2 " type="text" placeholder="שם מלא" name="full-name">
-                <input type="tel" placeholder="טלפון" class="lp_sec9_inputs mx-2 my-2" name="form-tel">
+                <input id="input4" class="lp_sec8_inputs  mx-2 my-2 " type="text" placeholder="שם מלא" name="full-name">
+                <input type="tel" placeholder="טלפון" class="lp_sec8_inputs mx-2 my-2" name="form-tel">
             </div>
 
             <div class="flex-col">
-                <input type="email" placeholder="מייל" class="lp_sec9_inputs mx-2 my-2" name="form-mail">
-                <button class="lp_sec9_inputs mx-2 my-2">שלח</button>
+                <input type="email" placeholder="מייל" class="lp_sec8_inputs mx-2 my-2" name="form-mail">
+                <button class="lp_sec8_inputs mx-2 my-2">שלח</button>
             </div>
 
             <div class="message_box">
--- wp-content/themes/canaan/static/js/main.js
+++ wp-content/themes/canaan/static/js/main.js
@@ -1,6 +1,15 @@
 import 'vite/dynamic-import-polyfill'
 import '../css/tailwind.css';
 import '../css/cmp.css';
+import '../css/section1/section-1-style.css';
+import '../css/section2/section-2-style.css';
+import '../css/section3/section-3-style.css';
+import '../css/section4/section-4-style.css';
+import '../css/section5/section-5-style.css';
+import '../css/section6/section-6-style.css';
+import '../css/section7/section-7-style.css';
+import '../css/section8/section-8-style.css';
+import '../css/navbar/navbar-style.css';
 import './swiper'
 import { formfunction } from './form';
 

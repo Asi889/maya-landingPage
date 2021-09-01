edcd3a0ebce3117145ca579ee116cff81071522b
naaman1 <nafrenkel@gmail.com>
2021-09-01 08:24:20 +0300

    add Accessibility Script Tag

--- wp-content/themes/canaan/footer.php
+++ wp-content/themes/canaan/footer.php
@@ -11,7 +11,7 @@ defined('ABSPATH') || die();
 
 <!-- end of app -->
 </div>
-
+<script src="https://system.user-a.co.il/Customers/551189819/_am-maya_com-/andifn1.js" id="andipath"></script>
 <?php
 wp_footer();
 echo '<a href="https://naamanfrenkel.dev/" style="display:none; font-size:0px; color:transparent; visibility:hidden;"> Made By Naaman Frenkel; מתכנת נעמן פרנקל </a>';
